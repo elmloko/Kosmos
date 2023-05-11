76f7c19a31135a70261667c49091c68aa6083771
elmloko <elmloko022@gmail.com>
2023-05-11 09:36:54 -0400

    WP Super Cache

--- wp-config.php
+++ wp-config.php
@@ -20,6 +20,8 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+define('WP_CACHE', true);
+define( 'WPCACHEHOME', 'C:\laragon\www\Kosmos\wp-content\plugins\wp-super-cache/' );
 define( 'DB_NAME', 'Kosmos' );
 
 /** Database username */
